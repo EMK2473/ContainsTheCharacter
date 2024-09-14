196a4402eba083bc0fe3e2bc8e18ada10be2bc23
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 16:04:58 -0400

    declared and assigned variables; string set to scnr next line; stringInput as string array splitting string by spaces; num of elements assigned by parsing integer at index 0 of string array; declared assigned new temp string array to hold values of NUM of elements; looped through and assigned new string array to string array index + 1 to account for int in array; ouput prints for visual/testing

--- ContainsTheCharacter.java
+++ ContainsTheCharacter.java
@@ -4,6 +4,23 @@ import java.util.Scanner;
 
 public class ContainsTheCharacter {
    public static void main(String[] args) {
-      /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+
+      String input = scnr.nextLine();
+      String[] stringInput = input.split(" ");
+
+      int NUM_ELEMENTS = Integer.parseInt(stringInput[0]);
+      char charInput = stringInput[stringInput.length - 1].charAt(0);
+
+      System.out.println(NUM_ELEMENTS);
+      System.out.println(charInput);
+
+      String[] newStringArray = new String[NUM_ELEMENTS];
+
+      for(int i = 0; i < NUM_ELEMENTS; ++i){
+        newStringArray[i] = stringInput[i + 1];
+        System.out.println(newStringArray[i]);
+      }
+
    }
 }
