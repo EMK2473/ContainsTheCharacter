2e518fc50c0e0072ee65b7d7788c476a8e2a3d0f
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 16:15:09 -0400

    removed testing output prints; formatted for submission

--- ContainsTheCharacter.java
+++ ContainsTheCharacter.java
@@ -12,14 +12,10 @@ public class ContainsTheCharacter {
       int NUM_ELEMENTS = Integer.parseInt(stringInput[0]);
       char charInput = stringInput[stringInput.length - 1].charAt(0);
 
-      System.out.println(NUM_ELEMENTS);
-      System.out.println(charInput);
-
       String[] newStringArray = new String[NUM_ELEMENTS];
 
       for(int i = 0; i < NUM_ELEMENTS; ++i){
         newStringArray[i] = stringInput[i + 1];
-        System.out.println(newStringArray[i]);
       }
 
       for(String s: newStringArray){
