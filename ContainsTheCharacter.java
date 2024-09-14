7c6ec9992949917dfaef87fc57f2f8160098bdfe
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-14 14:39:29 -0400

    init file

---
+++ ContainsTheCharacter.java
@@ -0,0 +1,9 @@
+package ContainsTheCharacter;
+
+import java.util.Scanner;
+
+public class ContainsTheCharacter {
+   public static void main(String[] args) {
+      /* Type your code here. */
+   }
+}
